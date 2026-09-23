f7c55807df1baabf5eea6c630f5cbe9c1017dc80
Manmath Sangave <74316187+manmath2005@users.noreply.github.com>
2026-09-23 20:43:59 +0530

    update: js/firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -12,11 +12,11 @@
  */
 
 export const firebaseConfig = {
-  apiKey: "REPLACE_WITH_YOUR_FIREBASE_API_KEY",
-  authDomain: "REPLACE_WITH_YOUR_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://REPLACE_WITH_YOUR_PROJECT_ID-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "REPLACE_WITH_YOUR_PROJECT_ID",
-  storageBucket: "REPLACE_WITH_YOUR_PROJECT_ID.appspot.com",
+  apiKey: "REPLACE_WITH_YOUR_FIREBASE_API_KEY", // Still needs to be updated
+  authDomain: "smart-helmet-tracker-bd08a.firebaseapp.com",
+  databaseURL: "https://smart-helmet-tracker-bd08a-default-rtdb.asia-southeast1.firebasedatabase.app", // or firebaseio.com
+  projectId: "smart-helmet-tracker-bd08a",
+  storageBucket: "smart-helmet-tracker-bd08a.appspot.com",
   messagingSenderId: "REPLACE_WITH_YOUR_MESSAGING_SENDER_ID",
   appId: "REPLACE_WITH_YOUR_APP_ID"
 };
